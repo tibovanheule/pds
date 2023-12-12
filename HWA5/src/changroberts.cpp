44f73406d8d054d76dccda5f76411b599e696c77
Tibo Vanheule <tibo@tibovanheule.space>
2023-12-12 04:01:20 +0100

    backup

--- HWA5/src/changroberts.cpp
+++ HWA5/src/changroberts.cpp
@@ -11,85 +11,107 @@
 void ChangRoberts::run(int argc, char **argv) const {
     Process thisProcess(argc, argv);
 
-    if (thisProcess.getNProc() <= 1) exit(-1);
+    if (thisProcess.getNProc() <= 1) {
+        std::cout << "Only one process: process with rank 0 is the leader." << std::endl;
+        return;
+    }
 
     // procedure initialize
     thisProcess.setPassive(); //participanti  = false;
     // end procedure
 
-    //procedure startElection
+    //procedure startElection; only for node 0
     if (thisProcess.getProcessRank() == 0) {
-        //SEND FIRST REQUEST, INITIATE ELECTION.
-        int send[] = {thisProcess.getProcessRank(), thisProcess.getUid()};
-
-        MPI_Send(send, 2, MPI_INT, 1, 1, MPI_COMM_WORLD);
+#ifdef DEBUG
+        std::cout << "Process 0 sending first request" << std::endl;
+#endif
         thisProcess.setActive();
-    }
 
-    /*
-    thisProcess.setElectedProcessUid(-1); // electedi = ?
-    if (thisProcess.isPassive()) { // if false then Election(i,IDi) is already been sent
-        // send message
-        int other_process = (thisProcess.getProcessRank() == thisProcess.getNProc() - 1) ? 0 :
-                        thisProcess.getProcessRank() + 1;
-        int send[] = {thisProcess.getProcessRank(), thisProcess.getUid()};
-        MPI_Send(send, 2, MPI_INT, other_process, 1, MPI_COMM_WORLD);
-        thisProcess.setActive(); // participation
-    }*/
+        int send[] = {0, thisProcess.getUid(), 1};
+        MPI_Send(send, 3, MPI_INT, 1, 1, MPI_COMM_WORLD);
+
+        thisProcess.setElectedProcessUid(-1);
+    }
     // end procedure
 
-    // procedure receiveElection(i,ID) . message received at pj
-    int recv[] = {0, 0};
+    MPI_Status status_with_tag;
+    int recv[3];
     int other_process = (thisProcess.getProcessRank() == 0) ? thisProcess.getNProc() - 1 :
-                        thisProcess.getProcessRank() - 1;
-    MPI_Recv(recv, 2, MPI_INT, other_process, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    int rank_other_process = recv[0];
-    int id_other_process = recv[1];
-    if (id_other_process > thisProcess.getUid()){
+                        thisProcess.getProcessRank() - 1; // id of sender.
+    MPI_Recv(recv, 3, MPI_INT, other_process, MPI_ANY_TAG, MPI_COMM_WORLD, &status_with_tag);
 
+    while (status_with_tag.MPI_TAG == 1) {
+        // procedure receiveElection(i,ID) . message received at pj
+#ifdef DEBUG
+        std::cout << "Received election, rank initiater: " << recv[0] << " id other process" << recv[1] << std::endl;
+#endif
+        int send_process = (thisProcess.getProcessRank() == thisProcess.getNProc() - 1) ? 0 :
+                           thisProcess.getProcessRank() + 1;
+
+        if (recv[1] > thisProcess.getUid()) {
+#ifdef DEBUG
+            std::cout << "id bigger, process" << thisProcess.getProcessRank() << std::endl;
+#endif
+            thisProcess.setElectedProcessUid(-1);
+            int send[] = {recv[0], recv[1], recv[2] + 1};
+            MPI_Send(send, 3, MPI_INT, send_process, 1, MPI_COMM_WORLD);
+            thisProcess.setActive();
+        }
+        if ((recv[1] <= thisProcess.getUid()) && (recv[0] != thisProcess.getProcessRank())) {
+#ifdef DEBUG
+            std::cout << "kleinere id, process " << thisProcess.getProcessRank() << std::endl;
+#endif
+            thisProcess.setElectedProcessUid(-1);
+            if (thisProcess.isPassive()) {
+                int send[] = {thisProcess.getProcessRank(), thisProcess.getUid(), recv[2] + 1};
+                MPI_Send(send, 3, MPI_INT, send_process, 1, MPI_COMM_WORLD);
+                thisProcess.setActive();
+            }
+        }
+        if (recv[0] == thisProcess.getProcessRank()) {
+#ifdef DEBUG
+            std::cout << "i==j, process " << thisProcess.getProcessRank() << std::endl;
+#endif
+            thisProcess.setPassive();
+            thisProcess.setElectedProcessUid(recv[1]);
+            int send[] = {thisProcess.getProcessRank(), thisProcess.getUid(), recv[2] };
+            MPI_Send(send, 3, MPI_INT, send_process, 0, MPI_COMM_WORLD);
+        }
+
+        MPI_Recv(recv, 3, MPI_INT, other_process, MPI_ANY_TAG, MPI_COMM_WORLD, &status_with_tag);
     }
-    
-    electedj ?
-    forward Election(i, ID)
-    participantj
-    true
-    end
-    if
-        if ID  IDj ^i
-    6 = j
-    then
-    electedj ?
-    if participantj == false
-    then
-    send Election(j, IDj)
-    participantj
-    true
-    end
-    if
-        end
-        if
-            if i == j
-    then
-            participantj
-    false
-    electedj j
-    send Elected(j)
-    end
-    if
-        end procedure
-        procedure
-        receiveElected(i).message
-    received
-    at pj
-    if i
-        6 = j
-    then
-            participantj
-    false
-    electedj i
-    forward Elected(i)
-    end
-    if
-        end procedure
+#ifdef DEBUG
+    std::cout << "out of while loop, process" << thisProcess.getProcessRank() << std::endl;
+#endif
+    // end procedure
+    if (status_with_tag.MPI_TAG == 0) {
+#ifdef DEBUG
+        std::cout << "Received elected node, rank initiater: " << recv[0] << " id other process" << recv[1]
+                  << std::endl;
+#endif
+        int send_process = (thisProcess.getProcessRank() == thisProcess.getNProc() - 1) ? 0 :
+                           thisProcess.getProcessRank() + 1;
+
+        if (recv[0] != thisProcess.getProcessRank()) {
+#ifdef DEBUG
+            std::cout << "Sending to" << send_process << " from " << thisProcess.getProcessRank()
+                      << std::endl;
+#endif
+            thisProcess.setPassive();
+            thisProcess.setElectedProcessUid(recv[1]);
+            //recv[2] += 1;
+            MPI_Send(recv, 3, MPI_INT, send_process, 0, MPI_COMM_WORLD);
+        } else {
+            std::cout << "I, process with rank " << thisProcess.getProcessRank() << " am the leader with UID: "
+                      << thisProcess.getUid()
+                      << "." << std::endl << "There were " << recv[2] << " messages sent to elect a leader."
+                      << std::endl;
+        }
+        // end procedure
+    } else {
+        exit(0);
+    }
+
+
     // No need to call MPI_Finalize, this is handled in the destructor of 'Process'. No need to explicitly call the destructor
 }
